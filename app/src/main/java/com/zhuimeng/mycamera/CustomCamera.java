3b1c361a8333bb48c38ac5fdce4d1a425c29fb8e
zhuimeng <guofeizouandroid@163.com>
2017-07-03 22:44:39 +0800

    second commit

---
+++ app/src/main/java/com/zhuimeng/mycamera/CustomCamera.java
@@ -0,0 +1,10 @@
+package com.zhuimeng.mycamera;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by find on 2017/7/3.
+ */
+
+public class CustomCamera extends AppCompatActivity {
+}
